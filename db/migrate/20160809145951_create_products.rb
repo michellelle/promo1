3808513792c09305a48f54832ea5f3a844ec39ff
michellelle <roxytoeshoe@gmail.com>
2016-08-09 23:42:42 +0000

    added plan object, plan model, and if statement

--- db/migrate/20160809145951_create_products.rb
+++ db/migrate/20160809145951_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.float :price
+      t.decimal :price
       t.string :picture
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809203942) do
+ActiveRecord::Schema.define(version: 20160809224827) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -21,10 +21,17 @@ ActiveRecord::Schema.define(version: 20160809203942) do
     t.datetime "updated_at"
   end
 
+  create_table "plans", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.float    "price"
+    t.decimal  "price"
     t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
