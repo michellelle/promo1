b3dc8332b5ac4d7467a7adea58c374a8e0de36f4
michellelle <roxytoeshoe@gmail.com>
2016-08-09 03:46:59 +0000

    added mailer form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,17 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         
         if @contact.save
-            redirect_to new_contact_path, notice: "Message sent."
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+            
+            ContactMailer.contact_email(name, email, body).deliver
+            
+            flash[:success] = "Message sent."
+            redirect_to new_contact_path
         else
-            redirect_to new_contact_path, notice: "Error occured."
+            flash[:danger] = "Error occured."
+            redirect_to new_contact_path
         end
     end
     
