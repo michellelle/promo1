2f191c5e007e47f34c5d74c6df9f6b3ffa9ee3b1
michellelle <roxytoeshoe@gmail.com>
2016-08-13 23:48:42 +0000

    created new home page and updated all styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
     def home
-        @basic_plan = Plan.find(1)
     end
     
     def about
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,3 @@
 class Product < ActiveRecord::Base
-    belongs_to :price
-    belongs_to :title
-    belongs_to :description
-    belongs_to :picture
+
 end
