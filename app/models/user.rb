6d6a1aa753ddc004a4332f21646bb30175e32790
michellelle <roxytoeshoe@gmail.com>
2016-08-16 00:04:40 +0000

    modified user record base

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
-  has_many :products
 end
